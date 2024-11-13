b0d88f56b68b02645cea094983c8f48ed41f81d4
Avijit <avijit@example.com>
2024-11-13 17:40:21 +0530

    4th commit

--- main.tf
+++ main.tf
@@ -1,12 +1,6 @@
 resource "aws_vpc" "user3vpc" {
   cidr_block = "10.111.3.0/24"
-
-<<<<<<< HEAD
-tags = {git 
+  tags = { 
   Name = "vpc from cli to HCP cloud-user3"
-=======
-tags = {
-  Name = "vpc from cli to HCP cloud user3"
->>>>>>> 38379b9caeafcd6277fde93c2eaef4c470d44700
   }
 }
